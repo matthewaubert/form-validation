a6511b7634ad2bc6fd29e3fe827879eb82aa5573
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-10-31 08:05:47 -0400

    Fill out common patterns in field checking funcs

--- script.js
+++ script.js
@@ -11,24 +11,49 @@ const formFields = {
 // functions to check each form field
 const checkInput = {
   email: function() {
-    // check if valid email address
-    // if field input is valid, remove error message
+    // define error messages
+    const errorMsg = {
+      empty: 'You must enter an email',
+      invalid: 'You must enter a valid email address',
+    };
+    // if field input is valid email address, remove error message
     if (this.validity.valid) {
-      resetError(this);
+      removeError(this);
     // if still an error, show error
+    } else {
+      showError(this, errorMsg.invalid);
+    }
+    // check if empty
+    checkEmpty(this, errorMsg.empty);
+  },
+  country: function() {
+    // define error messages
+    const errorMsg = {
+      empty: 'You must select a country',
+      invalid: '',
+    };
+    // check if one of 5 valid countries
+    const validCountries = ['us', 'ch', 'fr', 'de', 'nl'];
+    if (validCountries.includes(this.value)) {
+      removeError(this);
     } else {
       showError(this);
     }
     // check if empty
-    checkEmpty(this);
-  },
-  country: function() {
-    // check if one of 5 valid countries
-    // check if empty
-    checkEmpty(this);
+    checkEmpty(this, errorMsg.empty);
   },
   zip: function() {
-    // for each country, define regex pattern
+    // define error messages for each country
+    const errorMsg = {
+      empty: 'You must enter a ZIP code',
+      invalid: '',
+      us: 'U.S. ZIP codes must have exactly 5 digits',
+      ch: 'Switzerland ZIP codes must have exactly 4 digits: e.g. CH-1950 or 1950',
+      fr: 'France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012',
+      de: 'Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345',
+      nl: 'Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS'
+    };
+    // define regex pattern for each country
     const constraints = {
       us: 'd{5}$',
       ch: '^(CH-)?\\d{4}$',
@@ -36,50 +61,49 @@ const checkInput = {
       de: '^(D-)?\\d{5}$',
       nl: '^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$'
     };
-    // for each country, define errorMsg
-    const errorMsg = {
-      us: 'U.S. ZIP codes must have exactly 5 digits',
-      ch: 'Switzerland ZIP codes must have exactly 4 digits: e.g. CH-1950 or 1950',
-      fr: 'France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012',
-      de: 'Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345',
-      nl: 'Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS'
-    };
     // check if matches regex pattern for its relevant country
     // check if empty
-    checkEmpty(this);
+    checkEmpty(this, errorMsg.empty);
   },
   pwd: function() {
+    // define error messages
+    const errorMsg = {
+      empty: 'You must enter a password',
+      invalid: '',
+    };
     // check if matches password regex
     // check if too short
     // check if too long
     // check if empty
-    checkEmpty(this);
+    checkEmpty(this, errorMsg.empty);
   },
   confirm: function() {
+    // define error messages
+    const errorMsg = {
+      empty: 'You must confirm your password',
+      invalid: '',
+    };
     // check if matches password input
     // check if empty
-    checkEmpty(this);
+    checkEmpty(this, errorMsg.empty);
   }
 };
 
 // function to check if field is empty and show appropriate error message
-function checkEmpty(field) {
-  // errorMsg obj
-  const errorMsg = {
-    email: 'enter an email',
-    country: 'select a country',
-    zip: 'enter a ZIP code',
-    pwd: 'enter a password',
-    confirm: 'confirm your password'
-  };
-  
+function checkEmpty(field, errorMsg) {
+  // console.log(errorMsg);
   // if field is empty, display error message
   if (field.validity.valueMissing) {
-    field.nextElementSibling.textContent = `You must ${errorMsg[field.id]}.`;
+    showError(field, errorMsg);
   }
 }
 
-function resetError(field) {
+// function to display error message
+function showError(field, errorMsg) {
+  field.nextElementSibling.textContent = errorMsg;
+}
+
+function removeError(field) {
   field.nextElementSibling.textContent = ''; // reset content of message
 }
 
